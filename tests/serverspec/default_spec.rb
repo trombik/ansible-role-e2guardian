cca915a10655d04c120994ddb16b5cea7183ba99
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-03-28 21:03:46 +0900

    create e2guardianf1.conf

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -8,14 +8,18 @@ user    = "e2guardian"
 group   = "e2guardian"
 ports   = [ 8080 ]
 log_dir = "/var/log/e2guardian"
+default_user = "root"
+default_group = "root"
 
 case os[:family]
 when "freebsd"
   user = "nobody"
   group = "nobody"
   config_dir = "/usr/local/etc/e2guardian"
+  default_group = "wheel"
 end
 config = "#{ config_dir }/e2guardian.conf"
+e2guardianf1_conf = "#{ config_dir }/e2guardianf1.conf"
 
 describe package(package) do
   it { should be_installed }
@@ -24,6 +28,14 @@ end
 describe file(config) do
   it { should be_file }
   its(:content) { should match Regexp.escape("e2guardian") }
+end
+
+describe file(e2guardianf1_conf) do
+  it { should be_file }
+  it { should be_mode 644 }
+  it { should be_owned_by default_user }
+  it { should be_grouped_into default_group }
+  its(:content) { should match(/^groupname = 'Default group'$/) }
 end
 
 describe file(log_dir) do
